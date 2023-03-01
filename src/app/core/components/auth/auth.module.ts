f1dacbf887744462ddd8f10ba3e058c14bbd0785
Cleitonjr-dev <cleitonjr.dev@gmail.com>
2023-02-28 21:22:18 -0300

    criação de services

--- src/app/core/components/auth/auth.module.ts
+++ src/app/core/components/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Routing
 import { AuthRoutingModule } from './auth-routing.module';
+
+// Pages Component
 import { SignComponent } from './pages/sign/sign.component';
+
 
 
 @NgModule({
@@ -11,7 +16,9 @@ import { SignComponent } from './pages/sign/sign.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    
   ]
 })
 export class AuthModule { }
